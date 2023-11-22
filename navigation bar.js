afdb3e4f2b749c89941316eb3aa65f8079507de9
Akpali Francis Ezennaya <124525866+Akpali9@users.noreply.github.com>
2023-11-22 10:04:31 +0100

    Add files via upload

---
+++ navigation bar.js
@@ -0,0 +1,5 @@
+const togglebtn = document.querySelector('.toggle-button');
+togglebtn.onclick =function () {
+navbar = document.querySelector('.top');
+navbar.classList.toggle('active');    
+}      
